2a975c9dbca2c960b3574a52a7c57bd76ade8fbc
Maksim Kazantsev <maksimkazantsev2003@gmail.com>
2024-05-21 22:03:33 +0300

    start

--- example.go
+++ example.go
@@ -2,7 +2,6 @@ package cachery
 
 import (
 	"context"
-	"fmt"
 )
 
 type Modifier func(cache any)
@@ -14,22 +13,6 @@ type Cache interface {
 }
 
 // NewCache creates a new example of cache
-func NewCache(t cacheType, mods ...Modifier) Cache {
-	switch t {
-	case LRU:
-		return NewLRU(mods...)
-	case LFU:
-		return nil
-	default:
-		_ = fmt.Errorf("error wrong cache type: %s", t)
-		return nil
-	}
+func NewCache() Cache {
+	return &lru{}
 }
-
-type cacheType string
-
-const (
-	LFU          = "LFU"
-	LRU          = "LRU"
-	DefaultLimit = 10
-)
