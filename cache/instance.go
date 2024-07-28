4caa8cbd0b5667c279c71bd1e3cbe3a6c7290e53
Maksim Kazantsev <maksimkazantsev2003@gmail.com>
2024-07-28 20:46:18 +0300

    full cache remake, LRU cache ready, tests are included

---
+++ cache/instance.go
@@ -0,0 +1,24 @@
+package cache
+
+import "context"
+
+type Cache interface {
+	Get(ctx context.Context, key string) (any, bool)
+	Set(ctx context.Context, key string, val any)
+	Stop()
+}
+
+type cacheType string
+
+const (
+	LRU cacheType = "LRU"
+)
+
+func NewCache(cacheT cacheType, m ...ModifierFunc) Cache {
+	switch cacheT {
+	case LRU:
+		return NewLRU(m...)
+	default:
+		panic("unknown type of cache!")
+	}
+}
